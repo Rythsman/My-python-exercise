4ad5aa163fd17073fd44f73bd1640c65365eae05
wanghao <ryhs2018@gmail.com>
2018-03-11 23:08:03 +0800

    Update @property.py

--- 10.6前/@property.py
+++ 10.6前/@property.py
@@ -19,3 +19,8 @@ class Screen(object):
     @property
     def resolution(self):
         return self._height*self._width
+
+    
+1. Python中属性的获取是按照从下到上的顺序来查找属性；
+2. Python中的类和实例是两个完全独立的对象；
+3. Python中的属性设置是针对对象本身进行的；
